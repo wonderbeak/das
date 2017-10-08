b4e5aba0c6b84c7ec8c20b2ecec71f6e2ac078e9
Jaroslavs Rogačs <yaroslav.rogatch@gmail.com>
2017-10-08 18:16:58 +0300

    [DAS] Social project

---
+++ build/Pods.build/Debug-iphoneos/Pods-social.build/DerivedSources/Pods_social_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_socialVersionString[];
+ extern const double Pods_socialVersionNumber;
+
+ const unsigned char Pods_socialVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_social  PROJECT:Pods-1" "\n";
+ const double Pods_socialVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ social/Controller/SignInVC.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  social
+//
+//  Created by Jaroslavs Rogačs on 08/10/2017.
+//  Copyright © 2017 UTGARD.io. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+
+}
+
