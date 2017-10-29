7f423bed2322f03c3d6fe6dd1670a07675522b3a
Jaroslavs Rogacs <brugge@Odin.local>
2017-10-29 14:57:20 +0200

    Posts fetching

--- social/Controller/FeedVC.swift
+++ social/Controller/FeedVC.swift
@@ -14,25 +14,43 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        DataService.init().fetchPosts()
         
+        DataService.init().REF_POSTS.getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    let key = document.documentID
+                    let post = Post.init(postKey: key, postData: document.data())
+                    self.posts.append(post)
+                    //print("\(document.documentID) => \(document.data())")
+                }
+            }
+            self.tableView.reloadData()
+        }
+        //self.tableView.reloadData()
         //print(DataService.init().fetchPosts(uid: "46daiPsVmhArL0nx1JEf"))
         // Do any additional setup after loading the view.
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 3
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("JESS \(posts.count)")
+        return posts.count
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let post = posts[indexPath.row]
+        print("JESS \(post.caption)")
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
         
     }
@@ -42,7 +60,7 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBAction func signOutButton(_ sender: Any) {
         print("Sign out performed")
-        print(DataService.init().fetchPost(uid: "46daiPsVmhArL0nx1JEf"))
+        //print(DataService.init().fetchPost(uid: "46daiPsVmhArL0nx1JEf"))
         KeychainWrapper.standard.removeObject(forKey: KEY_UID)
         performSegue(withIdentifier: "goToSignIn", sender: nil)
     }
--- social/Services/DataService.swift
+++ social/Services/DataService.swift
@@ -11,10 +11,10 @@ import Firebase
 
 class DataService {
     
-    private var REF_USERS = Firestore.firestore().collection("users")
-    private var REF_POSTS = Firestore.firestore().collection("posts")
-    private var REF_COMMENTS = Firestore.firestore().collection("comments")
-    private var REF_IMAGES = Firestore.firestore().collection("images")
+    public var REF_USERS = Firestore.firestore().collection("users")
+    public var REF_POSTS = Firestore.firestore().collection("posts")
+    public var REF_COMMENTS = Firestore.firestore().collection("comments")
+    public var REF_IMAGES = Firestore.firestore().collection("images")
     //FirebaseApp.configure()
     
     // USERS
@@ -32,16 +32,23 @@ class DataService {
     func fetchPost(uid: String) -> Any {
         return REF_POSTS.document(uid)
     }
-    func fetchPosts() -> Any {
-        return REF_POSTS.getDocuments() { (querySnapshot, err) in
+    
+    var posts = [Post]()
+    
+    func fetchPosts() -> [Post] {
+        REF_POSTS.getDocuments() { (querySnapshot, err) in
             if let err = err {
                 print("Error getting documents: \(err)")
             } else {
                 for document in querySnapshot!.documents {
+                    let key = document.documentID
+                    let post = Post.init(postKey: key, postData: document.data())
+                    self.posts.append(post)
                     print("\(document.documentID) => \(document.data())")
                 }
             }
         }
+        return posts
     }
 //
 //    .getDocument{ (document, error) in
